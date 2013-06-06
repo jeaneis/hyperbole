e000b410246c1e88fd5df8e02e85ad9dbb3bff12
justinek <justinek@stanford.edu>
2013-06-06 09:09:34 -0700

    posted mturk hit for page experiment

---
+++ code/mTurkExp/hyperboleExp_constrained_page/hyperbole_constrained_page1-postHIT.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+pushd /Applications/aws-mturk-clt-1.3.0/bin
+./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /Users/justinek/Documents/Grad_school/Research/Hyperbole/hyperbole_github/code/mTurkExp/hyperboleExp_constrained_page//hyperbole_constrained_page1 -input /Users/justinek/Documents/Grad_school/Research/Hyperbole/hyperbole_github/code/mTurkExp/hyperboleExp_constrained_page//hyperbole_constrained_page1.input -question /Users/justinek/Documents/Grad_school/Research/Hyperbole/hyperbole_github/code/mTurkExp/hyperboleExp_constrained_page//hyperbole_constrained_page1.question -properties /Users/justinek/Documents/Grad_school/Research/Hyperbole/hyperbole_github/code/mTurkExp/hyperboleExp_constrained_page//hyperbole_constrained_page1.properties -maxhits 1
+popd
